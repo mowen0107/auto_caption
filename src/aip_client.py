5012dd74544ef93d2b2214a404b750bdac39368a
黄子廷 <huangziting0107@126.com>
2018-05-28 10:05:53 +0800

    成功调用google翻译的api

--- src/aip_client.py
+++ src/aip_client.py
@@ -3,6 +3,7 @@
     Create Date: 20180522
 '''
 from aip import AipSpeech
+from googletrans import Translator
 from src.config import AIP_CONFIG
 
 
@@ -11,20 +12,30 @@ class AipClient():
         APP_ID = AIP_CONFIG['APP_ID']
         API_KEY = AIP_CONFIG['API_KEY']
         SECRET_KEY = AIP_CONFIG['SECRET_KEY']
-        self.client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
+        self.audio_recognition_client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
+        self.translation_client = Translator(
+            service_urls=['translate.google.cn'])
 
     def recognize_audio(self, audio_data):
         try:
-            response_json = self.client.asr(audio_data, 'wav', 16000, {
-                'dev_pid': 1737
-            })
+            response_json = self.audio_recognition_client.asr(
+                audio_data, 'wav', 16000, {
+                    'dev_pid': 1737
+                })
         except Exception as err:
             print("--- AipClient,get_recognition_result,", err)
             raise
         else:
             if response_json['err_no'] == 0:
                 result = response_json['result'][0]
+                # 返回最佳识别结果的字符串
                 return result
             else:
                 print("err_no:{}".format(response_json['err_no']))
                 return "met error when recognized audio"
+
+    def translate(self, str_to_trans):
+        trans_result = self.translation_client.translate(
+            str_to_trans, dest='zh-CN').text
+        print("translation_result:{}".format(trans_result))
+        return trans_result
--- test/test_aip_client.py
+++ test/test_aip_client.py
@@ -25,12 +25,16 @@ class TestAipClient():
     def test_get_recognition_result(self):
         result_list = []
         for i in range(0, 17):
-            test_audio_path = "./resource/audio/test_video/fragment_{}.wav".format(
+            audio_path = "./resource/audio/test_video/fragment_{}.wav".format(
                 i)
-            with open(test_audio_path, 'rb') as fp:
+            with open(audio_path, 'rb') as fp:
                 audio_data = fp.read()
             result = self.aip_client.recognize_audio(audio_data)
             print("--- TestAipClient,test_get_recognition_result, result:{}".
                   format(result))
             result_list.append(result)
         print("result_list:{}".format(result_list))
+
+    def test_translate(self):
+        trans_str = self.aip_client.translate("hello world")
+        assert trans_str == "你好，世界"
